4fdf64f70076aa4996dc472af328e6a1ca4db20a
Krystal Guzman <kryguzman@gmail.com>
2020-03-12 10:43:21 -0700

    added motd

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require("dotenv").config(); 
+//read from a .env file located at the root of the project
+//merge key/value pairs
 const server = require('./api/server.js');
 //make the port assigned by the server
 //heroku will place the .PORT environment value on their server
